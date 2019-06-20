ca093d09676cda5b722553c2ae40025b5150996f
chenchuwei8888 <51996574+chenchuwei8888@users.noreply.github.com>
2019-06-20 10:32:47 +0800

    Update aaa.java

--- src/main/java/com/mybatictest/demo/controller/aaa.java
+++ src/main/java/com/mybatictest/demo/controller/aaa.java
@@ -3,7 +3,7 @@ package com.mybatictest.demo.controller;
 public class aaa {
    ccccccccccccc
    bbbbbbbbbbbbb
-
+ int a ;
     public ccccccccccccc getBbbbbbbbbbbbb() {
         return bbbbbbbbbbbbb;
     }
