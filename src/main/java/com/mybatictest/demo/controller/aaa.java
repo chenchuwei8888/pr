92ca86d3a42ca2fc4b2c0ef3dba061226fd6c96c
mypr <1242588909@qq.com>
2019-06-20 10:33:03 +0800

    55

--- src/main/java/com/mybatictest/demo/controller/aaa.java
+++ src/main/java/com/mybatictest/demo/controller/aaa.java
@@ -3,7 +3,7 @@ package com.mybatictest.demo.controller;
 public class aaa {
    ccccccccccccc
    bbbbbbbbbbbbb
-
+    String a;
     public ccccccccccccc getBbbbbbbbbbbbb() {
         return bbbbbbbbbbbbb;
     }
